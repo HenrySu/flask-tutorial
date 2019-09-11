166b78efbcb688ce8043f6d6175e36dfdbb0af0c
HenrySU <suhang@hotmail.com>
2019-09-11 16:11:38 +0800

    database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev',
@@ -19,5 +20,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello world!'
+
+    from . import db
+    db.init_app(app)
 
     return app
